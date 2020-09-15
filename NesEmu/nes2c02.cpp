af34c2012cf88bee1d19df36196e3cd03d0b636f
pearlescen7 <basketuzay@hotmail.com>
2020-09-15 17:53:42 +0300

    Added some definitions for 2c02.

--- NesEmu/nes2c02.cpp
+++ NesEmu/nes2c02.cpp
@@ -1,4 +1,5 @@
 #include "nes2c02.h"
+#include "Cartridge.h"
 
 void nes2c02::insertCartridge(std::shared_ptr<Cartridge> cartridge)
 {
@@ -10,15 +11,92 @@ void nes2c02::cpuWrite(uint16_t addr, uint8_t data)
 
 uint8_t nes2c02::cpuRead(uint16_t addr)
 {
+
 	return 0;
 }
 
 void nes2c02::ppuWrite(uint16_t addr, uint8_t data)
-{}
+{
+	addr &= 0x3FFF;
+
+	if(cartridge->ppuWrite(addr, data)){}
+	else if (addr >= 0x0000 && 0x1FFF)
+	{
+		patternTable[(addr & 0x1000) >> 12][addr & 0x0FFF] = data;
+	}
+	else if (addr >= 0x2000 && addr <= 0x3EFF)
+	{
+		addr &= 0x0FFF;
+		if (cartridge->mirror == Cartridge::Mirror::VERTICAL)
+		{
+			if (addr >= 0x0000 && addr <= 0x03FF) nameTable[0][addr & 0x03FF] = data;
+			if (addr >= 0x0400 && addr <= 0x07FF) nameTable[1][addr & 0x03FF] = data;
+			if (addr >= 0x0800 && addr <= 0x0BFF) nameTable[0][addr & 0x03FF] = data;
+			if (addr >= 0x0C00 && addr <= 0x0FFF) nameTable[1][addr & 0x03FF] = data;
+		}
+		else if (cartridge->mirror == Cartridge::Mirror::HORIZONTAL)
+		{
+			if (addr >= 0x0000 && addr <= 0x03FF) nameTable[0][addr & 0x03FF] = data;
+			if (addr >= 0x0400 && addr <= 0x07FF) nameTable[0][addr & 0x03FF] = data;
+			if (addr >= 0x0800 && addr <= 0x0BFF) nameTable[1][addr & 0x03FF] = data;
+			if (addr >= 0x0C00 && addr <= 0x0FFF) nameTable[1][addr & 0x03FF] = data;
+		}
+	}
+	else if (addr >= 0x3F00 && addr <= 0x3FFF)
+	{
+		addr &= 0x001F;
+
+		if (addr == 0x0010) addr = 0x0000;
+		if (addr == 0x0014) addr = 0x0004;
+		if (addr == 0x0018) addr = 0x0008;
+		if (addr == 0x001C) addr = 0x000C;
+		paletteTable[addr] = data;
+	}
+}
 
 uint8_t nes2c02::ppuRead(uint16_t addr)
 {
-	return 0;
+	uint8_t temp = 0x00;
+	addr &= 0x3FFF;
+	
+	if (cartridge->ppuRead(addr, temp)) {}
+	else if (addr >= 0x0000 && addr <= 0x1FFF)
+	{
+		//if cartridge can't map
+		temp = patternTable[(addr & 0x1000) >> 12][addr & 0x0FFF];
+	}
+	else if (addr >= 0x2000 && addr <= 0x3EFF)
+	{
+		//check cartridge mirroring to access name table
+		addr &= 0x0FFF;
+		if (cartridge->mirror == Cartridge::Mirror::VERTICAL)
+		{
+			if (addr >= 0x0000 && addr <= 0x03FF) temp = nameTable[0][addr & 0x03FF];
+			if (addr >= 0x0400 && addr <= 0x07FF) temp = nameTable[1][addr & 0x03FF];
+			if (addr >= 0x0800 && addr <= 0x0BFF) temp = nameTable[0][addr & 0x03FF];
+			if (addr >= 0x0C00 && addr <= 0x0FFF) temp = nameTable[1][addr & 0x03FF];
+		}
+		else if (cartridge->mirror == Cartridge::Mirror::HORIZONTAL)
+		{
+			if (addr >= 0x0000 && addr <= 0x03FF) temp = nameTable[0][addr & 0x03FF];
+			if (addr >= 0x0400 && addr <= 0x07FF) temp = nameTable[0][addr & 0x03FF];
+			if (addr >= 0x0800 && addr <= 0x0BFF) temp = nameTable[1][addr & 0x03FF];
+			if (addr >= 0x0C00 && addr <= 0x0FFF) temp = nameTable[1][addr & 0x03FF];
+		}
+	}
+	else if (addr >= 0x3F00 && addr <= 0x3FFF)
+	{
+		//use palette memory
+		addr &= 0x001F;
+		//addr refers to bg color
+		if (addr == 0x0010) addr = 0x0000;
+		if (addr == 0x0014) addr = 0x0004;
+		if (addr == 0x0018) addr = 0x0008;
+		if (addr == 0x001C) addr = 0x000C;
+		temp = paletteTable[addr];
+	}
+
+	return temp;
 }
 
 void nes2c02::clock()
--- NesEmu/nes2c02.h
+++ NesEmu/nes2c02.h
@@ -8,13 +8,18 @@ class nes2c02
 {
 private:
 	std::shared_ptr<Cartridge> cartridge;
+	uint8_t nameTable[2][1024];
+	uint8_t patternTable[2][4096];
+	uint8_t paletteTable[32];
 
 public:
 	void insertCartridge(std::shared_ptr<Cartridge> cartridge);
 
+	//ppu to main bus 
 	void cpuWrite(uint16_t addr, uint8_t data);
 	uint8_t cpuRead(uint16_t addr);
 
+	//ppu to ppu bus
 	void ppuWrite(uint16_t addr, uint8_t data);
 	uint8_t ppuRead(uint16_t addr);
 
